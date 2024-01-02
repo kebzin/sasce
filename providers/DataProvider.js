bbabbd19b2b3d0046c4fa8da52ec0f726174ae27
kebzin <honorablewaiga@gmail.com>
2024-01-02 21:09:04 +0000

    Ajust the behavioure of the Data fetching
    Signed-off-by: kebzin <honorablewaiga@gmail.com>

--- providers/DataProvider.js
+++ providers/DataProvider.js
@@ -1,42 +1,82 @@
-import { StyleSheet, Text, View } from "react-native";
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useEffect, useRef, useState } from "react";
 import { products } from "../constants/dummyData";
 import { supabase } from "../lib/superbase";
 
 export const DataProviderContes = createContext();
+
 export const DataProvider = ({ children }) => {
-  const [ItemList, SetITemList] = useState(products);
+  const [itemList, setItemList] = useState([]);
   const [filter, setFilter] = useState("All category");
   const [card, setCard] = useState([]);
-  const [sample, setsample] = useState();
+  const [error, setError] = useState();
+  const [isError, setIsError] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const [refreshing, setRefreshing] = useState(false);
 
-  // function to featch the data
+  const isFetchingDataRef = useRef(false); // Use ref to track data fetching
+
   const fetchData = async () => {
+    if (isFetchingDataRef.current) {
+      // If data fetching is already in progress, return early
+      return;
+    }
+    setRefreshing(true);
+    setIsError(false);
+    isFetchingDataRef.current = true; // Set flag to indicate data fetching is in progress
+
     try {
-      const { data, error } = await supabase.from("product").select("*");
+      const { data, error } = await supabase
+        .from("product")
+        .select("*")
+        .order("created_at", { ascending: false, nullsFirst: false })
+
+        .range(itemList.length, itemList.length + 1 * itemList.length + 10)
+        .limit(pageSize);
+
       if (error) {
-        console.log(error);
+        setError(error.message);
+        setIsError(false);
+        setRefreshing(false);
+
+        console.log("error", error);
       }
-      setsample(data);
+
+      setItemList((prevData) => [...prevData, ...(data || [])]);
+      setRefreshing(false);
     } catch (error) {
       console.error(error);
+      setRefreshing(false);
+    } finally {
+      setRefreshing(false);
+      isFetchingDataRef.current = false; // Reset the data fetching flag
     }
   };
+
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [currentPage]);
 
-  console.log(sample);
+  const handleOnEndReach = async () => {
+    setIsError(false);
+    await fetchData();
+  };
+
   return (
     <DataProviderContes.Provider
       value={{
-        ItemList,
+        itemList,
         filter,
         card,
-        sample,
         setCard,
         setFilter,
-        SetITemList,
+        refreshData: fetchData,
+        handleOnEndReach,
+        error,
+        setError,
+        isError,
+        setIsError,
+        refreshing,
       }}
     >
       {children}
